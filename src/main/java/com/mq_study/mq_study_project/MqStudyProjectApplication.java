2b17b44ac4ac2b2651c592ee06ff5dcb7a0767cd
RoterLong <1726357945@qq.com>
2025-08-10 16:52:54 +0800

    first commit

---
+++ src/main/java/com/mq_study/mq_study_project/MqStudyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mq_study.mq_study_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqStudyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqStudyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mq_study/mq_study_project/MqStudyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mq_study.mq_study_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqStudyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
